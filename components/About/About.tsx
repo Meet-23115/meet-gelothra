74a3ff75761bbd412213ba11eb5a49ab5f58bb37
Meet Gelothra <imailmeet4000@gmail.com>
2026-08-04 20:28:56 +0530

    resume button working

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -2,11 +2,12 @@
 import { DiaTextReveal } from "@/components/ui/dia-text-reveal";
 import { WavyBackground } from "@/components/ui/wavy-background";
 import React from "react";
+import { LiquidGlassCard } from "../uilayouts/liquid-glass";
 
 const About = () => {
   return (
     <div className="About" id="about">
-      <WavyBackground  className="max-w-4xl mx-auto pb-40">
+      <WavyBackground className="max-w-4xl flex flex-col gap-[10px] mx-auto pb-40">
         <p className=" text-white headline-text font-bold inter-var text-center">
           <DiaTextReveal
             className="  text-white font-bold tracking-tight"
@@ -22,6 +23,27 @@ const About = () => {
           is an opportunity to learn something new, solve meaningful problems,
           and create products that leave a lasting impression.
         </p>
+        <div className="Resume-btn-container">
+          <a
+            href="/Meet-Gelothra.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block h-full p-4"
+          >
+            <div className="cursor-pointer">
+              <LiquidGlassCard
+                glowIntensity="xl"
+                shadowIntensity="xl"
+                borderRadius="50px"
+                blurIntensity="sm"
+                draggable={false}
+                className="resume-liquid-glass"
+              >
+                <div>Resume</div>
+              </LiquidGlassCard>
+            </div>
+          </a>
+        </div>
         {/* Download button removed from About per request; moved to Nav */}
       </WavyBackground>
     </div>
